0a14206b057080e3a776493670bd2e72d048f7cd
ApazaVictor <167140778+ApazaVictor@users.noreply.github.com>
2024-12-05 13:52:37 -0500

    05/12/24

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+//Manejo de errores
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+//Cargar el archivo de configuración 
+require_once 'config/config.php';
+
+//Autoload de clases
+spl_autoload_register(function ($class_name) {
+    $directories = [
+        'controllers/',
+        'models/',
+        'config/',
+        'utils/',
+        ''
+    ];
+    
+    foreach ($directories as $directory) {
+        $file = $directory . $class_name . '.php';
+        if (file_exists($file)) {
+            // var_dump($file);
+            require_once $file;
+            return;
+        }
+    }
+});
+
+//Crear una instancia el router
+$router = new Router();
+
+$public_routes = [
+    '/web',
+    '/login',
+    '/register',
+];
+
+//Obtener la ruta actual
+$current_route = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$current_route = str_replace (dirname($_SERVER['SCRIPT_NAME']), '', $current_route);
+//$current_route = lA RUTA DESPUES DE LA CARPETA PROYECTO.
+//echo $current_route;
+//var_dump(dirname($_SERVER['SCRIPT_NAME']);
+//var_dump($current_route);
+//die($$current_route);
+
+
+$router->add('GET','/web','WebController', 'index');
+
+//Login and register
+$router->add('GET','/login','AuthController', 'showLogin');
+$router->add('GET','/register','AuthController', 'showRegister');
+
+$router->add('POST','auth/login','AuthController', 'login');
+$router->add('POST','auth/register','AuthController', 'register');
+
+
+
+//Despachar la ruta
+try {
+    $router->dispatch($current_route, $_SERVER['REQUEST_METHOD']);
+} catch (Exception $e) {
+    // Manejar el error
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        include 'views/errors/404.php';
+    } else {
+        header('Content-Type: application/json');
+        echo json_encode([
+            'status' => 'error',
+            'message' => $e->getMessage()
+        ]);
+    }
+}
+
+
+
+
+
+//include "./config/Database.php";
+
+
+//$db = new Database();
+//$valida = $db -> connect();
+
+//if ($valida) {
+    //echo "Connexion establecida correctamente";
+//}else{
+   // echo "Connexion no establecida";
+//}
+
---
+++ views/errors/404.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina no encontrado</title>
+    <style>
+        body {
+            display: flex; /* Utiliza flexbox para centrar el contenido */
+            flex-direction: column; /* Coloca los elementos en una columna */
+            align-items: center; /* Centra los elementos horizontalmente */
+            justify-content: center; /* Centra los elementos verticalmente */
+            height: 100vh; /* Hace que el cuerpo ocupe toda la altura de la ventana */
+            margin: 0; /* Elimina el margen por defecto del body */
+        }
+
+        img {
+            max-width: 100%; /* Asegura que la imagen no exceda el ancho de la pantalla */
+            height: auto; /* Mantiene la proporción de la imagen */
+        }
+    </style>
+</head>
+<body>
+    <h1>Pagina no encontrado Error 404</h1>
+    <div>
+        <div>
+            <img width="404" src="https://kfg6bckb.media.zestyio.com/yalantis-interactive-404.gif"alt=""srcset="">
+        </div>
+    </div>
+</body>
+</html>
